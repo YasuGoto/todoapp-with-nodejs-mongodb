b637a52cdbe776c882d3c55f543e6f425c09db4b
YasuGoto <yasuko.goto@nijibox.co.jp>
2025-02-02 14:43:17 +0900

    フロントエンド側の処理実装

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const PORT = 3000;
 const taskRoute = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
+
 app.use(express.json());
+app.use(express.static("./public"));
 
 // ルーティング設定
 app.use("/api/v1/tasks", taskRoute);
