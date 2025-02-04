f884ee9d2a90c3b8aa60467fd19550510cfded54
YasuGoto <1998y.i@gmail.com>
2025-02-04 15:16:38 +0900

    ログ追加とpath変更

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = 3000;
 const taskRoute = require("./routes/tasks");
@@ -6,13 +7,33 @@ const connectDB = require("./db/connect");
 require("dotenv").config();
 
 app.use(express.json());
-app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static("./public"));
 
 // ルーティング設定
 app.use(
-  "https://todoapp-with-nodejs-mongodb-bg20.onrender.com/api/v1/tasks",
+  "/api/v1/tasks",
   taskRoute
 );
+
+app.get('/', async (_, res) => {
+  try {
+    res.status(200).json({ message: "success" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// 404ハンドラー
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Not Found" });
+});
+
+// エラーハンドラー
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error" });
+});
 
 // DB接続
 const start = async () => {
